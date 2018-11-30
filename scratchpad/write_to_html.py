ce1afcbfe2350ee20989ffc7237364a9438d99a1
Brian Lynch <bglynch17@gmail.com>
2018-11-30 12:29:27 +0000

    created basic homepage sitemap
    created python file in scratchpad for writing to html
    pasted in current datasets
    wrote data from datsets to html file
    code left at bottom of python file for future improvements on writing data to html

---
+++ scratchpad/write_to_html.py
@@ -0,0 +1,150 @@
+URL = 'https://wiprodigital.com'
+
+internal_links = {
+    'https://wiprodigital.com',
+    'https://wiprodigital.com/2018/08/09/turning-data-into-consumer-growth',
+    'https://wiprodigital.com/2018/10/04/three-ways-to-navigate-your-data-science-journey',
+    'https://wiprodigital.com/2018/11/15/making-health-insurance-conversational-insights-from-chatbot-design',
+    'https://wiprodigital.com/2018/11/20/the-meaning-of-motion',
+    'https://wiprodigital.com/2018/11/27/field-service-automation-the-rx-for-rising-customer-expectations',
+    'https://wiprodigital.com/2018/11/29/the-portal-experience',
+    'https://wiprodigital.com/case-study-redefining-newborn-care-for-volusense',
+    'https://wiprodigital.com/cases/a-design-system-to-enable-future-growth',
+    'https://wiprodigital.com/cases/case-study-speaking-from-the-heart',
+    'https://wiprodigital.com/cases/delivering-an-exceptional-mortgage-customer-experience-for-allied-irish-bank',
+    'https://wiprodigital.com/cases/in24',
+    'https://wiprodigital.com/cases/progressive-metering-a-utilitys-strategic-move-into-predictive-planning',
+    'https://wiprodigital.com/designit-approach',
+    'https://wiprodigital.com/events/join-wipro-digital-for-two-iot-panels-at-world-economic-forum-wef',
+    'https://wiprodigital.com/events/meet-wipro-digital-at-pega-customer-engagement-summit-2018',
+    'https://wiprodigital.com/events/nate-clinton-managing-director-cooper-at-sf-design-week-2018',
+    'https://wiprodigital.com/events/teresa-brazen-senior-director-cooper-at-design-thinking-2018',
+    'https://wiprodigital.com/events/wipro-digital-designit-at-idc-european-digital-executive-summit-2018',
+    'https://wiprodigital.com/events/wipro-digital-is-a-platinum-sponsor-at-springone-platform-2018',
+    'https://wiprodigital.com/get-in-touch',
+    'https://wiprodigital.com/join-our-team',
+    'https://wiprodigital.com/news/new-survey-highlights-leadership-crisis-digital-transformation',
+    'https://wiprodigital.com/news/pacs-european-trend-study-digital-industrial-revolution-with-predictive-maintenance',
+    'https://wiprodigital.com/news/the-state-of-martech-study',
+    'https://wiprodigital.com/news/wipro-digital-acquire-cooper-leader-ux-interaction-design-expand-designits-capabilities',
+    'https://wiprodigital.com/news/wipro-named-global-leader-digital-transformation-strategy-consulting',
+    'https://wiprodigital.com/news/wipro-recognized-as-a-leader-in-digital-process-automation-by-independent-research-firm',
+    'https://wiprodigital.com/privacy-policy',
+    'https://wiprodigital.com/what-we-do',
+    'https://wiprodigital.com/what-we-think',
+    'https://wiprodigital.com/who-we-are'
+}
+
+external_links={
+    'https://twitter.com/wiprodigital',
+    'https://www.facebook.com/WiproDigital',
+    'https://www.linkedin.com/company/wipro-digital'
+}
+
+image_links = {
+    '"https://s17776.pcdn.co/wp-content/themes/wiprodigital/images/logo-dk.png"',
+    '"https://s17776.pcdn.co/wp-content/themes/wiprodigital/images/logo.png"',
+    '"https://s17776.pcdn.co/wp-content/uploads/2016/03/home-cta.png"',
+    '"https://s17776.pcdn.co/wp-content/uploads/2016/06/blueprint-e1464977489331-1024x578.png"',
+    '"https://s17776.pcdn.co/wp-content/uploads/2016/06/blueprint-e1464977489331.png"',
+    '"https://s17776.pcdn.co/wp-content/uploads/2016/08/Fav_icon_144x144.png"',
+    "'https://s17776.pcdn.co/wp-content/uploads/2016/03/Cases-Designit-Call-Me.png'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2016/03/Cases-Designit-Volusense.png'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2016/03/Survey-New-Survey-Highlights-Leadership-Crisis-in-Digital-Transformation.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2016/06/Cases-In24.png'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2016/08/Case-Study-Progressive-Metering-A-Utility’s-Strategic-Move-into-Predictive-Planning.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2016/09/Blueprint-Original.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2017/09/News_Wipro-Named-Global-Leader.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2017/10/News-Wipro-Digital-to-acquire-Cooper-3.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/01/Events-Join-Wipro-for-two-IoT-panels-at-World-Economic-Forum-WEF.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/03/Events-Teresa-Brazen-Senior-Director-Cooper-at-Design-Thinking-2018-2.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/04/Events-Nate-Clinton-Managing-Director-Cooper-at-SF-Design-Week-2018.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/05/News-New-MarTech-Study.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/06/Case-Study-A-Design-System-to-Enable-Future-Growth.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/06/Events-Wipro-Digital-Designit-at-IDC-European-Digital-Executive-Summit-2018.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/06/Insights-PACs-European-Trend-Study-Digital-Industrial-Revolution-with-Predictive-Maintenance.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/08/Events-Wipro-Digital-is-a-Platinum-Sponsor-at-SpringOne-Platform-2018-1.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/08/Insights-Turning-Data-Into-Consumer-Growth.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/09/Events-Meet-Wipro-at-PEGA-Customer-Engagement-Summit-2018.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/09/Insights-Three-ways-to-navigate-your-data-science-journey.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/10/Case-Study-Delivering-an-Exceptional-Mortgage-Customer-Experience-for-Allied-Irish-Bank.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/11/Insights-Field-service-automation-the-Rx-for-rising-customer-expectations.png'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/11/Insights-Making-Health-Insurance-Conversational-Insights-from-Chatbot-Design.jpg'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/11/Insights-The-meaning-of-motion-1.gif'",
+    "'https://s17776.pcdn.co/wp-content/uploads/2018/11/Insights-The-portal-experience.png'"
+}
+
+css_links=[
+    'https://s17776.pcdn.co/wp-content/themes/wiprodigital/style.css?ver=1533667894'
+    ]
+
+js_links=[
+    'https://s17776.pcdn.co/wp-content/themes/wiprodigital/scripts/jquery-1.12.0.min.js',
+    'https://s17776.pcdn.co/wp-content/themes/wiprodigital/scripts/bootstrap.min.js',
+    'https://s17776.pcdn.co/wp-content/themes/wiprodigital/scripts/inview.js',
+    'https://s17776.pcdn.co/wp-content/themes/wiprodigital/scripts/modernizr.min.js',
+    'https://s17776.pcdn.co/wp-content/themes/wiprodigital/scripts/jquery.slicknav.min.js',
+    'https://s17776.pcdn.co/wp-content/themes/wiprodigital/scripts/wiprodigital.js',
+    'https://s17776.pcdn.co/wp-content/themes/wiprodigital/scripts/mason.js',
+    'https://s17776.pcdn.co/wp-content/themes/wiprodigital/scripts/wdhome.js',
+    'https://s17776.pcdn.co/wp-includes/js/jquery/jquery.js?ver=1.12.4',
+    'https://s17776.pcdn.co/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1',
+    'https://s17776.pcdn.co/wp-content/plugins/wp-scroll-depth/js/jquery-scrolldepth/jquery.scrolldepth.min.js?ver=4.9.8',
+    '//js.hsforms.net/forms/v2.js',
+    '//js.hs-scripts.com/621527.js',
+    'https://s17776.pcdn.co/wp-includes/js/wp-embed.min.js?ver=4.9.8'
+    ]
+
+
+        
+
+    
+# PRINT THE ABOVE LISTS TO A HTML FILE IN UNORDERED LISTS
+with open('scratchpad/sitemap.html', 'w') as myFile:
+    myFile.write('<html>')
+    myFile.write('<body>')
+    myFile.write('<h1>Sitemap for https://wiprodigital.com</h1>')
+    myFile.write('<h2>Internal Links</h2>')
+    
+    for url in sorted(internal_links):
+        myFile.write(f"<ul><a href={url}><li>{url.replace(URL, '')}</li></a></ul>")
+
+    myFile.write('<h2>External Links</h2>')
+    for url in sorted(external_links):
+        myFile.write(f"<ul><a href='{url}'><li>{url}</li></a></ul>")
+    
+    myFile.write('<h2>Static Links</h2>')
+    
+    myFile.write('<h3>Image Links</h2>')
+    for url in sorted(image_links):
+        myFile.write(f"<ul><a href={url}><li>{url}</li></a></ul>")
+    
+    myFile.write('<h3>CSS Links</h2>')
+    for url in css_links:
+        myFile.write(f"<ul><a href='{url}'><li>{url}</li></a></ul>")
+    
+    myFile.write('<h3>Javascript Links</h2>')
+    for url in js_links:
+        myFile.write(f"<ul><a href='{url}'><li>{url}</li></a></ul>")
+    
+    myFile.write('</body>')
+
+# ==========================================
+# future code for improving the html display
+# ==========================================
+
+# get different link levels
+branch_depths = []
+for url in internal_links:
+    if url.count('/') not in branch_depths:
+        branch_depths.append(url.count('/'))
+branch_depths.sort()
+
+
+# Top level links
+# for url in internal_links:
+#     if url.count('/') == branch_depths[0]:
+#         print(url.split('/')[-1])
+#         myFile.write(
+#             f"<ul><a href='{url}'><li>{url.split('/')[-1]}</li></a></ul>"
+#             )
