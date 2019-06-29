768085dc5c43973ec134ae4644d3b95c445ebde9
Brian Lynch <bglynch17@gmail.com>
2019-06-29 23:05:21 +0000

    c9 closing

--- scratchpad/get_all_urls.py
+++ scratchpad/get_all_urls.py
@@ -24,12 +24,60 @@ from webcrawler_test import (
     format_html_to_xml_soup,
     get_all_links_from_url,
     )
+import pprint
 
 domain = 'https://wiprodigital.com/'
 
 # Create sets
+start_urls = get_all_links_from_url(domain)
 all_urls = set()
 crawled_urls = set()
-uncrawled_urls = set()
 external_urls = set()
+
+# Parse out the external links
+for link in start_urls:
+    if 'https://wiprodigital.com' in link:
+        all_urls.add(link)
+    else:
+        external_urls.add(link)
+
+uncrawled_urls = all_urls - crawled_urls
+
+print(len(all_urls))
+print('')
+print(len(external_urls))
+print('')
+print(len(uncrawled_urls))
+
+while len(uncrawled_urls) > 0:
+    i = 0
+    for link in uncrawled_urls:
+        i += 1
+        print(f"{i} / {len(uncrawled_urls)}: {link}")
+        link_urls = get_all_links_from_url(link)
+        crawled_urls.add(link)
+        for url in link_urls:
+            if 'https://wiprodigital.com' in url:
+                all_urls.add(url)
+            else:
+                external_urls.add(url)
+            
+    uncrawled_urls = all_urls - crawled_urls
+    print(uncrawled_urls)
     
+    print(len(all_urls))
+    print('')
+    print(len(external_urls))
+    print('')
+    print(len(uncrawled_urls))
+
+
+
+print('')
+print('========== INTERNAL URLS ============')
+print('')
+pprint.pprint(sorted(all_urls))
+print('')
+print('========== EXTERNAL URLS ============')
+print('')
+pprint.pprint(sorted(external_urls))
